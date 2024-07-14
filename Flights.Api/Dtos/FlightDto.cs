b2b3d46476f62b66777704d77d56ae76fdc6daa8
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-14 16:56:20 -0500

    add some into to flights dto

--- Flights.Api/Dtos/FlightDto.cs
+++ Flights.Api/Dtos/FlightDto.cs
@@ -1,6 +1,9 @@
 ﻿namespace Flights.Api.Dtos;
 
-public record class FlightDto
-{
-  
-}
+public record class FlightDto(
+  int Id,
+  string Airline,
+  string Airport,
+  int FlightNumber,
+  DateOnly Departs
+  )
